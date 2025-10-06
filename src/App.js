a024049e9b6f15a06d36e976426927efee23cb26
= <mprobertson95@gmail.com>
2025-10-06 15:57:54 +0700

    React 1 - First commit, removing unnecessary files and playing with jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import jsxRuntime from 'react/jsx-runtime';
 
 function App() {
+  var name = "React Learner";
+
+  const handleClick = () => {
+    name = "Matt Did this";
+    alert('Button clicked!');
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +16,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h1>Hello {name}</h1>
         </a>
+        <button onClick={handleClick}>Update Text</button>
       </header>
     </div>
   );
